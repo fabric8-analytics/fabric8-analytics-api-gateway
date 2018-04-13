b8816d28ed732bedf262f3ae47bb359285d19152
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-13 13:13:55 +0200

    Stub for unit tests

---
+++ tests/test_api_v1.py
@@ -0,0 +1,8 @@
+"""Unit tests for the authorization module."""
+
+import unittest
+from gateway import api_v1
+
+
+if __name__ == '__main__':
+    pass
---
+++ tests/test_auth.py
@@ -0,0 +1,8 @@
+"""Unit tests for the authorization module."""
+
+import unittest
+from gateway import auth
+
+
+if __name__ == '__main__':
+    pass
