4a030b79cb76f114a6d3cb04a0fedf3a478974ac
Tomáš Hrčka <tomas.hrcka@gmail.com>
2018-04-17 19:52:14 +0200

    Update cico_setup.sh
    Remove whitespace

--- cico_setup.sh
+++ cico_setup.sh
@@ -14,7 +14,7 @@ prep() {
     yum -y update
     yum -y install epel-release
     yum -y install gcc python34-pip python34-requests python34-devel docker git which python34-virtualenv
-   	pip3 install pytest
+    pip3 install pytest
     pip3 install docker-compose
     systemctl start docker
 }
