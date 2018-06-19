c2bf60b8cca5d879b03770b55f67d42da12c816d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-19 13:36:19 +0200

    Fixed issues in scripts to run tests and to list TODOs

--- runtest.sh
+++ runtest.sh
@@ -8,15 +8,32 @@ set -e
 # unable to prepare context: The Dockerfile (Dockerfile.tests) must be within the build context (.)
 set -x
 
-echo "Create Virtualenv for Python deps ..."
+export TERM=xterm
+TERM=${TERM:-xterm}
+
+# set up terminal colors
+NORMAL=$(tput sgr0)
+RED=$(tput bold && tput setaf 1)
+GREEN=$(tput bold && tput setaf 2)
+YELLOW=$(tput bold && tput setaf 3)
+
+printf "%sCreate Virtualenv for Python deps ..." "${NORMAL}"
+
 function prepare_venv() {
-    VIRTUALENV=`which virtualenv`
-    if [ $? -eq 1 ]; then
+    VIRTUALENV=$(which virtualenv)
+    if [ $? -eq 1 ]
+    then
         # python34 which is in CentOS does not have virtualenv binary
-        VIRTUALENV=`which virtualenv-3`
+        VIRTUALENV=$(which virtualenv-3)
     fi
 
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt && python3 `which pip3` install -r test_requirements.txt
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt && python3 "$(which pip3)" install -r test_requirements.txt
+    if [ $? -ne 0 ]
+    then
+        printf "%sPython virtual environment can't be initialized%s" "${RED}" "${NORMAL}"
+        exit 1
+    fi
+    printf "%sPython virtual environment initialized%s\n" "${YELLOW}" "${NORMAL}"
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
@@ -40,5 +57,5 @@ echo "*** Unit tests ***"
 echo "*****************************************"
 
 echo "Starting test suite"
-DISABLE_AUTHENTICATION=1 PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` --cov=gateway/ --cov-report term-missing -vv tests
-echo "Test suite passed \\o/"
+DISABLE_AUTHENTICATION=1 PYTHONDONTWRITEBYTECODE=1 python3 "$(which pytest)" --cov=gateway/ --cov-report term-missing -vv tests
+printf "%stests passed%s\n\n" "${GREEN}" "${NORMAL}"
